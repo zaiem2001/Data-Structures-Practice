dd03b2b3a3d7ff0cb00eeafe90ecb43f42724d4d
zaiem2001 <zaiem.180410116045@gmail.com>
2024-06-28 10:50:17 -0400

    java(problem)

---
+++ Java/Problems/ShippingTask.java
@@ -0,0 +1,33 @@
+package Java.Problems;
+
+public class ShippingTask {
+
+    public static long minimalNumberOfPackages(long items, long availableLargePackages, long availableSmallPackages) {
+        final long LARGE_PACKAGE_SIZE = 5;
+        long maxWeight = (LARGE_PACKAGE_SIZE * availableLargePackages) + availableSmallPackages;
+        long totalBoxes = 0;
+
+        if (maxWeight < items)
+            return -1;
+
+        while (availableLargePackages != 0) {
+            if (items < LARGE_PACKAGE_SIZE) {
+                totalBoxes++;
+                availableLargePackages--;
+                items = 0;
+            }
+            if (items == 0)
+                break;
+
+            items -= LARGE_PACKAGE_SIZE;
+            totalBoxes++;
+            availableLargePackages--;
+        }
+
+        return totalBoxes + items;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(minimalNumberOfPackages(16, 3, 10)); // Output should be 3
+    }
+}
