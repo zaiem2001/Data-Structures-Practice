c655c94f1acd331a0a2c15cc1f0978fb4ff155f7
zaiem2001 <zaiem.180410116045@gmail.com>
2024-09-07 11:51:43 -0400

    java(helper) recursive linkedlist reverse

--- Java/AtoZ/LinkedList/Reverse.java
+++ Java/AtoZ/LinkedList/Reverse.java
@@ -18,4 +18,21 @@ public class Reverse {
 
         return prev;
     }
+
+    public static ListNode<Integer> reverse_recursive(ListNode<Integer> head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        // Recursively reverse the rest of the list
+
+        ListNode<Integer> newHead = reverseList(head.next);
+
+        // Reverse the current node's pointer
+        head.next.next = head;
+        head.next = null;
+
+        // Return the new head of the reversed list
+        return newHead;
+    }
 }
