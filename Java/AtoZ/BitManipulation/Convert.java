f3f3bfc98ecbb98fb351eb5abdaf50153bc441db
zaiem2001 <zaiem.180410116045@gmail.com>
2024-09-14 09:01:20 -0400

    java(BitManipulation)

---
+++ Java/AtoZ/BitManipulation/Convert.java
@@ -0,0 +1,59 @@
+package Java.AtoZ.BitManipulation;
+
+public class Convert {
+    static String decimalToBinary(int x) {
+        if (x == 0)
+            return "0";
+
+        StringBuilder binary = new StringBuilder();
+
+        while (x > 0) {
+            if (x == 1) {
+                binary.append(1);
+                break;
+            }
+            int remainder = x % 2;
+            binary.append(remainder);
+            x /= 2;
+        }
+        binary.reverse();
+
+        return binary.toString();
+    }
+
+    static int binaryToDecimal(String binary) {
+        int decimal = 0;
+        int pow = 1;
+
+        if (binary.length() == 1)
+            return Integer.parseInt(binary);
+
+        for (int i = binary.length() - 1; i >= 0; i--) {
+            int number = Integer.parseInt(String.valueOf(binary.charAt(i)));
+
+            if (number == 1) {
+                decimal += pow;
+            }
+
+            pow *= 2;
+        }
+
+        return decimal;
+    }
+
+    static void swap(int a, int b) {
+        System.out.println(a + " " + b);
+        a = a ^ b;
+        b = a ^ b;
+        a = a ^ b;
+        System.out.println("After swapping...");
+        System.out.println(a + " " + b);
+    }
+
+    public static void main(String[] args) {
+        int x = 11;
+        System.out.println(decimalToBinary(x));
+        System.out.println(binaryToDecimal(decimalToBinary(x)));
+        swap(11, 7);
+    }
+}
